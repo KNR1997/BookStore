93d1973d337ff2173bbe067a8953dc7331e81baf
KNR1997 <kethakaranasinghe@gmail.com>
2023-04-09 06:40:42 +0530

    BS : findByBookId API call

--- src/main/java/com/bookstore/springboot/library/dao/ReviewRepository.java
+++ src/main/java/com/bookstore/springboot/library/dao/ReviewRepository.java
@@ -1,9 +1,13 @@
 package com.bookstore.springboot.library.dao;
 
 import com.bookstore.springboot.library.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    Page<Review> findByBookId(@RequestParam("book_Id") Long bookId, Pageable pageable);
 
 }
