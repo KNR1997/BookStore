1b8e74ed378ab300c5fa5899683e8e8e1ba1722d
KNR1997 <kethakaranasinghe@gmail.com>
2023-05-07 02:46:47 +0530

    BS : Renew Loan Service

--- src/main/java/com/bookstore/springboot/library/service/BookService.java
+++ src/main/java/com/bookstore/springboot/library/service/BookService.java
@@ -5,6 +5,7 @@ import com.bookstore.springboot.library.dao.CheckoutRepository;
 import com.bookstore.springboot.library.entity.Book;
 import com.bookstore.springboot.library.entity.Checkout;
 import com.bookstore.springboot.library.responsemodels.ShelfCurrentLoansResponse;
+import com.nimbusds.oauth2.sdk.util.date.SimpleDate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -112,5 +113,24 @@ public class BookService {
 
         bookRepository.save(book.get());
         checkoutRepository.deleteById(validateCheckout.getId());
+    }
+
+    public void renewLoan(String userEmail, Long bookId) throws Exception {
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if (validateCheckout == null) {
+            throw new Exception("Book does not exist or not checked out by user");
+        }
+
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date d1 = sdFormat.parse(validateCheckout.getReturnDate());
+        Date d2 = sdFormat.parse(LocalDate.now().toString());
+
+        if (d1.compareTo(d2) > 0 || d1.compareTo(d2) == 0) {
+            validateCheckout.setReturnDate(LocalDate.now().plusDays(7).toString());
+            checkoutRepository.save(validateCheckout);
+        }
     }
 }
