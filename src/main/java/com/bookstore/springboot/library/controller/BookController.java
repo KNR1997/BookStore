23511289b1ef30dc3af36d749233bb8f56cc77f4
KNR1997 <kethakaranasinghe@gmail.com>
2023-05-07 02:50:58 +0530

    BS : hot fix

--- src/main/java/com/bookstore/springboot/library/controller/BookController.java
+++ src/main/java/com/bookstore/springboot/library/controller/BookController.java
@@ -48,7 +48,7 @@ public class BookController {
     }
 
     @PutMapping("/secure/return")
-    public void returnBook(@RequestHeader(value = "Authorization") String token, \
+    public void returnBook(@RequestHeader(value = "Authorization") String token,
                            @RequestParam Long bookId) throws Exception {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         bookService.returnBook(userEmail, bookId);
