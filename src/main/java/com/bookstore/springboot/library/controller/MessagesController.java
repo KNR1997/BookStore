877926904fd1374dd2a6a52990a8d50bc44d78ef
KNR1997 <kethakaranasinghe@gmail.com>
2023-05-10 13:35:39 +0530

    BS : Enhance Message controller endpoint

--- src/main/java/com/bookstore/springboot/library/controller/MessagesController.java
+++ src/main/java/com/bookstore/springboot/library/controller/MessagesController.java
@@ -1,10 +1,13 @@
 package com.bookstore.springboot.library.controller;
 
 import com.bookstore.springboot.library.entity.Message;
+import com.bookstore.springboot.library.requestmodels.AdminQuestionRequest;
 import com.bookstore.springboot.library.service.MessagesService;
 import com.bookstore.springboot.library.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayDeque;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -24,4 +27,16 @@ public class MessagesController {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
     }
+
+    @PostMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
+    }
+
 }
