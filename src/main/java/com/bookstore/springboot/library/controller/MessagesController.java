bdfd5d5871033d91217352c6743ae99cc896dfa7
KNR1997 <kethakaranasinghe@gmail.com>
2023-05-10 08:29:39 +0530

    BS : Message endpoint created

---
+++ src/main/java/com/bookstore/springboot/library/controller/MessagesController.java
@@ -0,0 +1,27 @@
+package com.bookstore.springboot.library.controller;
+
+import com.bookstore.springboot.library.entity.Message;
+import com.bookstore.springboot.library.service.MessagesService;
+import com.bookstore.springboot.library.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/messages")
+public class MessagesController {
+
+    private MessagesService messagesService;
+
+    @Autowired
+    public MessagesController(MessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token,
+                            @RequestBody Message messageRequest) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messagesService.postMessage(messageRequest, userEmail);
+    }
+}
